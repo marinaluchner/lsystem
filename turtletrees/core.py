0f5a897e6ab200433d203082fb6b347d073bff5d
Jacob <jacob.wright@dtc.ox.ac.uk>
2022-10-24 15:54:54 +0100

    Modify core.py

--- turtletrees/core.py
+++ turtletrees/core.py
@@ -1,12 +1,23 @@
-# -*- coding: utf-8 -*-
-from . import helpers
+import tkinter as tk
 
-def get_hmm():
-    """Get a thought."""
-    return 'hmmm...'
+window = tk.Tk()
 
+# Create LHS frame for inputs
+frm_input = tk.Frame(master=window)
 
-def hmm():
-    """Contemplation..."""
-    if helpers.get_answer():
-        print(get_hmm())
+frm_entry = tk.Frame(master=frm_input)
+
+lbl_angle = tk.Label(master=frm_entry, text="Angle: ")
+lbl_angle.pack(side=tk.LEFT)
+
+ent_angle = tk.Entry(master=frm_entry, textvariable=10)
+ent_angle.pack(side=tk.RIGHT)
+
+frm_entry.pack(fill=tk.BOTH, expand=tk.YES)
+
+frm_input.pack(fill=tk.BOTH, side=tk.LEFT, expand=tk.YES)
+
+frm_output = tk.Frame(master=window)
+frm_output.pack(fill=tk.BOTH, side=tk.RIGHT, expand=tk.YES)
+
+window.mainloop()
