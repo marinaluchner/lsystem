d7a54b62f76ee1c798a0a12f6cf781ba9d308ed8
mwhitemfldm <miawhitefield@gmail.com>
2022-10-25 15:51:40 +0100

    add preselction box

--- turtletrees/OOP_core.py
+++ turtletrees/OOP_core.py
@@ -1,6 +1,7 @@
 import turtle
-import tkinter as tk
 import string
+from tkinter import ttk
+import tkinter as tk
 
 import lsystem
 class App:
@@ -27,31 +28,39 @@ class App:
         self.frameA = tk.Frame()
         self.frameA.pack(fill=tk.BOTH, side=tk.LEFT, expand=tk.YES)
         self.frameA.columnconfigure([0, 1], minsize=40)
-        self.frameA.rowconfigure([0, 1, 2], minsize=20) 
+        self.frameA.rowconfigure([0, 1, 2, 3, 4, 5], minsize=20) 
         
         # angle input
         self.lbl_angle = tk.Label(master=self.frameA, text="Angle: ")
         self.ent_angle = tk.Entry(master=self.frameA)
-        self.ent_angle.insert(0, '10')
+        self.ent_angle.insert(0, '20')
         self.lbl_angle.grid(row=0, column=0, sticky="nesw")
         self.ent_angle.grid(row=0, column=1, sticky="nesw")
         
         # length input
         self.lbl_length = tk.Label(master=self.frameA, text="Length: ")
         self.ent_length = tk.Entry(master=self.frameA)
-        self.ent_length.insert(0, '5')
+        self.ent_length.insert(0, '15')
         self.lbl_length.grid(row=1, column=0, sticky="nesw")
         self.ent_length.grid(row=1, column=1, sticky="nesw")
         
         # iteractions slider
         self.lbl_iters = tk.Label(master=self.frameA, text="Iterations: ")
         self.scl_iters = tk.Scale(master=self.frameA, from_=0, to=5, orient=tk.HORIZONTAL)
-        self.lbl_iters .grid(row=2, column=0, sticky="nesw")
-        self.scl_iters .grid(row=2, column=1, sticky="nesw")
+        self.lbl_iters.grid(row=2, column=0, sticky="nesw")
+        self.scl_iters.grid(row=2, column=1, sticky="nesw")
+        
+        # Preselction list
+        self.axiom_label = tk.Label(master=self.frameA, text="Select an organic structure")
+        self.axiom_label.grid(row=3, column=1,columnspan=2, sticky="nesw")
+        self.preselects = ttk.Combobox(
+            state="readonly",
+            values=["Python", "C", "C++", "Java", "Custom"])
+        self.preselects.grid(row=4, column=1,columnspan=2, sticky="nesw")
         
         # Go button
         self.btn_go = tk.Button(master=self.frameA, text="Go!", command=self.execute)
-        self.btn_go.grid(row=3, column=1, columnspan=2, sticky="nesw")
+        self.btn_go.grid(row=5, column=1, columnspan=2, sticky="nesw")
     
     def create_turtle_screen(self):
         
