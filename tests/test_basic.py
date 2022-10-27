0be3599eb06cf46790da721990c83a1191a243a6
Jacob <jacob.wright@dtc.ox.ac.uk>
2022-10-27 11:43:51 +0100

    Add random test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,6 +1,7 @@
 from turtletrees import core
 import unittest
-from unittest.mock import Mock
+import random
+
 
 class TheoryTest(unittest.TestCase):
     """
@@ -15,6 +16,12 @@ class TheoryTest(unittest.TestCase):
         """ Tests generate function using tree preset """
         self.assertEqual(core.generate('A+[A]B-', 2, 'B+[[A]-A]-B[-BA]+A', 'BA'),
                          'BA+[[B+[[A]-A]-B[-BA]+A]-B+[[A]-A]-B[-BA]+A]-BA[-BAB+[[A]-A]-B[-BA]+A]+B+[[A]-A]-B[-BA]+A+[BA+[[B+[[A]-A]-B[-BA]+A]-B+[[A]-A]-B[-BA]+A]-BA[-BAB+[[A]-A]-B[-BA]+A]+B+[[A]-A]-B[-BA]+A]BAB+[[A]-A]-B[-BA]+A-')
+   
+        # Checks that changing inp_string is same as running generate function for more iterations
+        n = random.randint(1, 6)
+        inp_string = core.generate('A+[A]B-', n, 'B+[[A]-A]-B[-BA]+A', 'BA')
+        self.assertEqual(core.generate(inp_string, 1, 'B+[[A]-A]-B[-BA]+A', 'BA'),
+                         core.generate('A+[A]B-', n+1, 'B+[[A]-A]-B[-BA]+A', 'BA'))
 
     def test_maxDepth(self):
         """ Tests maxDepth function using tree present output (max_iter=2) """
