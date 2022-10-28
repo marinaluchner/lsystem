a0c90fa5f5f53cd236a9ca9d9a418a65fadba9c7
Jacob <jacob.wright@dtc.ox.ac.uk>
2022-10-28 12:26:56 +0100

    Move Azure

--- turtletrees/core.py
+++ turtletrees/core.py
@@ -399,7 +399,7 @@ preset_dict = {'Custom':   {'angle': 12,
 if __name__ == '__main__':
     root = tk.Tk()
     # Set theme and font
-    root.tk.call("source", "Azure-ttk-theme-main/azure.tcl")
+    root.tk.call("source", "../Azure-ttk-theme-main/azure.tcl")
     root.tk.call("set_theme", "dark")
     root.option_add('*Font', 'Helvetica 12')
     app = App(root)
