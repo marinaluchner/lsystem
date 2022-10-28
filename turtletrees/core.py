91be1efe397776ae14cfe293577c67d965994bb2
Jacob <jacob.wright@dtc.ox.ac.uk>
2022-10-28 11:20:51 +0100

    Update core

--- turtletrees/core.py
+++ turtletrees/core.py
@@ -305,7 +305,7 @@ preset_dict = {'Custom':   {'angle': 12,
                             'axiom': 'A+[A]B-',
                             'start_color': (0, 0.6, 0.3),
                             'final_color': (0.6, 1, 1)},
-                'Algea':   {'angle': 22.5, 
+                'Algae':   {'angle': 22.5, 
                             'length': 10,
                             'max_iter': 4,
                             'ruleA': 'AA-[-A+A+A]+[+A-A-A]',
@@ -335,7 +335,7 @@ preset_dict = {'Custom':   {'angle': 12,
 if __name__ == '__main__':
     root = tk.Tk()
     # Set theme and font
-    root.tk.call("source", "Azure-ttk-theme/azure.tcl")
+    root.tk.call("source", "Azure-ttk-theme-main/azure.tcl")
     root.tk.call("set_theme", "dark")
     root.option_add('*Font', 'Helvetica 10')
     app = App(root)
